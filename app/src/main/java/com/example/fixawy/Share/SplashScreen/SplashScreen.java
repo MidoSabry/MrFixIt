3bf009736e744722b1314f6b0167653fb781e2ca
weaam906 <weamkamal450@gmail.com>
2021-06-01 02:10:01 +0200

    first commit

--- app/src/main/java/com/example/fixawy/Share/SplashScreen/SplashScreen.java
+++ app/src/main/java/com/example/fixawy/Share/SplashScreen/SplashScreen.java
@@ -2,9 +2,14 @@ package com.example.fixawy.Share.SplashScreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.fixawy.R;
+import com.example.fixawy.Share.Homes.OwnerHome;
+import com.example.fixawy.Share.LoginPage.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -12,5 +17,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            new Handler().postDelayed(() -> {
+                startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+                finish();
+
+            },2000);
+        }else {
+            new Handler().postDelayed(() -> {
+                startActivity(new Intent(SplashScreen.this, OwnerHome.class));
+                finish();
+
+            },2000);
+        }
     }
 }
